414cd689f5e2291b8abfa5f08c974428a66dac3e
María Benito <mariabenito@protonmail.com>
2017-05-30 01:04:52 -0300

    Update pipeline_someplots.py

--- pipeline_someplots.py
+++ pipeline_someplots.py
@@ -25,7 +25,7 @@ kbin = [0.00254, 0.00661, 0.01068, 0.01475, 0.01882, 0.02289, 0.02696, 0.03104,
 kbin = np.asarray(kbin)
 
 # Load CAMB spectrum
-datafile = '/Users/maria/Desktop/structure formation/code/data/cosmo_project_matterpower.dat'
+datafile = 'cosmo_project_matterpower.dat'
 Pk  = np.loadtxt(datafile,unpack=True)
 kth = Pk[0]
 Pth = Pk[1]
@@ -42,7 +42,7 @@ p0_th = Pth*(1+(2/3.)*beta+(1/5.)*beta**2)
 # Theoretic quadrupole
 p2_th = Pth*((4/3.)*beta+(4/7.)*beta**2)
 
-with open('/Users/maria/Dropbox/camb/data/Proj_multipoles_11.dat', 'r') \
+with open('Proj_multipoles.dat', 'r') \
 as f: 
     # Monopole
     p0 = np.zeros((number_k, number_simu))
@@ -68,7 +68,7 @@ as f:
                 #print Data
             i += 1
 
-with open('/Users/maria/Dropbox/camb/data/Proj_multipoles_12.dat', 'r') \
+with open('Proj_multipoles_1.dat', 'r') \
 as f: 
     # Define if line corresponds to a monopole or a quadrupole
     i    = 0 
